2a7685617a0ba05863288a932738c3a1b1798f9b
Sean Aubrey <seanpaubrey@gmail.com>
2018-02-02 02:05:49 -0500

    Transports .jpg
    -JPG files are now able to be opened and are copied correctly.
    -More sliding window redundancy logic added.
    -Final packet having less bytes than the generic packet size is now
    accounted for server AND client-side.
    -Other miscellanea

--- UDPFileTransfer/src/filetransfer/Client.java
+++ UDPFileTransfer/src/filetransfer/Client.java
@@ -18,22 +18,27 @@ public class Client {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("CLIENT: Enter a port number (Leave blank for default): ");
 		String p = scan.nextLine();
-		System.out.println("CLIENT: Port set.");
 		if (p.equals("")) {
 			p = "9870";
 		}
+		if (!checkPortInput(Integer.parseInt(p))) {
+			scan.close();
+			System.exit(0);
+		}
+		System.out.println("CLIENT: Port set.");
 		int port = Integer.parseInt(p);
 		
 		System.out.println("CLIENT: Enter an IP address (Leave blank for default): ");
 		String ip = scan.nextLine();
-		System.out.println("CLIENT: IP address set.");
 		if (ip.equals("")) {
 			ip = "127.0.0.1";
 		}
+		System.out.println("CLIENT: IP address set.");
 		
 		System.out.println("CLIENT: Enter desired file name: ");
 		String fileName = scan.nextLine();
 		System.out.println("CLIENT: File name set.");
+		
 		scan.close();
 		
 		String fileExtension = extractExtension(fileName);
@@ -43,6 +48,7 @@ public class Client {
 		int ackNum = 0;
 		int packetSize = 0; // Keep 0. Packet size received from Server.
 		int totalPackets = 0;
+		int totalBytes = 0;
 		
 		try {
 			DatagramChannel c = DatagramChannel.open();
@@ -58,7 +64,7 @@ public class Client {
 			byte[] b = new byte[buffer.remaining()];
 			buffer.get(b);
 			DatagramPacket send = new DatagramPacket(b, b.length, ia, port);
-			byte[] ack = new byte[8]; // two ints
+			byte[] ack = new byte[12]; // two ints
 			DatagramPacket rec = new DatagramPacket(ack, ack.length);
 			int timeouts = 0;
 			
@@ -80,30 +86,43 @@ public class Client {
 					}
 				}
 				// get ack for file name delivery
-				buffer = ByteBuffer.allocate(8);
+				buffer = ByteBuffer.allocate(12);
 				buffer.put(ack); // ack goes into buffer
 				buffer.flip();
 				packetSize = buffer.getInt();
 				totalPackets = buffer.getInt();
+				totalBytes = buffer.getInt();
 				if (packetSize > 0) {
 					System.out.println("CLIENT: File name ACK received.");
 					System.out.println("packet size: " + packetSize);
 					System.out.println("total packets: " + totalPackets);
+					System.out.println("total bytes: " + totalBytes);
 					gotFile = true;
 				}
 			}
-			
+			/*
+			 * To avoid writing the last packet as the size of packetSize, 
+			 * Client can calculate: packets * packetSize to get totalBytes.
+			 * totalBytes % packetSize gives remaining bytes for last packet. 
+			 */
 			packets = new byte[totalPackets][];
 			while (true) {
 				buffer = ByteBuffer.allocate(packetSize + 4);
 				byte[] pack = new byte[packetSize + 4];
 				DatagramPacket recPack = new DatagramPacket(pack, pack.length);
+
 				try {
-					ds.setSoTimeout(10000);
+					ds.setSoTimeout(100);
 					ds.receive(recPack);
 				} catch (SocketTimeoutException er) {
-					System.out.println("Timeout");
-					break;
+					if (ackNum != totalPackets - 1) {
+						sendAck(ackNum, ia, port);
+						System.out.println("TIMEOUT: Resent ACK#: " + ackNum);
+					}
+					else if (ackNum == totalPackets - 1) {
+						System.out.println("Transfer complete!");
+						break;
+					}
 					//printFile(packets, fileExtension);
 				}
 				
@@ -111,53 +130,48 @@ public class Client {
 					/*
 					 ackNum is the largest packet number received before a
 					 gap. This preserves order. 
-					 * Client can still receive packets greater than ackNum, however, 
-					 * so when inserting new packets, make sure not to just add them on to
-					 * the end of the array so that their sequence num matches their indice.
-					 * (seqNum is (packetSize - 1), indice matches sequence num.)
+					 * Client can still receive packets greater than ackNum
 					 */
 					buffer.put(pack);
 					int seqNum = buffer.getInt(0);
 					System.out.println("CLIENT: Packet# " + seqNum + " received.");
-					//System.out.println("seqNum: " + seqNum);
-				
+					
+					// If at final packet, get rid of empty bytes between 
+					// remaining bytes and the generic packet size.
+					if (seqNum == totalPackets - 1) {
+						buffer = trimExcessBytes(buffer, totalBytes, packetSize);
+					}
+					
 					// Store rest of packet
 					buffer.position(4); // skip integer
+					System.out.println(buffer.remaining());
 					b = new byte[buffer.remaining()];
 					buffer.get(b);
-					// skip insertion if it's already there.
-					if (packets[seqNum] == null) { // out of bounds, seqNum needs to be + 1!
+					
+					// skip insertion if packet was previously added.
+					if (packets[seqNum] == null) {
 						packets[seqNum] = b;
 					}
 					
 					// Determine ackNum
-					for (int i = ackNum; i < packets.length - 1; i++) {
+					for (int i = ackNum; i < packets.length; i++) {
 						if (packets[i] == null) {
-							ackNum = i;
+							ackNum = i - 1;
 							break;
+						} else { // if all packets have arrived, no nulls
+							ackNum = packets.length - 1;
 						}
 					}
 					
-					// TO-DO: Write to file AFTER COMPLETION
-					
+					// Write to file AFTER COMPLETION
 					FileOutputStream out = new FileOutputStream("newFile" + fileExtension, true);
-					String str = new String(b); 
-					byte[] by = str.getBytes();
-					out.write(by);
+					out.write(b);
 					out.close();
-					
+
 					//printFile(packets, fileExtension);
 					
 					//send acknowledgement
-					buffer = ByteBuffer.allocate(4);
-					buffer.putInt(ackNum); // ACK# as determined by sent seq num should go here
-					buffer.flip();
-					buffer.rewind();
-					byte[] outBytes = new byte[buffer.remaining()];
-					buffer.get(outBytes);
-					send = new DatagramPacket(outBytes, outBytes.length, ia, port);
-					ds.send(send);
-					System.out.println("CLIENT: Packet# " + (ackNum) + " ACK sent.");
+					sendAck(ackNum, ia, port);
 				}
 			}
 		} catch (IOException e) {
@@ -165,6 +179,44 @@ public class Client {
 		}
 	} // end main
 	
+	private static void sendAck(int ackNum, InetAddress ia, int port) {
+		try {
+			DatagramSocket ds = new DatagramSocket();
+			ByteBuffer buffer = ByteBuffer.allocate(4);
+			buffer.putInt(ackNum); // ACK# as determined by sent seq num should go here
+			buffer.flip();
+			buffer.rewind();
+			byte[] outBytes = new byte[buffer.remaining()];
+			buffer.get(outBytes);
+			DatagramPacket send = new DatagramPacket(outBytes, outBytes.length, ia, port);
+			ds.send(send);
+			System.out.println("CLIENT: Packet# " + (ackNum) + " ACK sent.");
+			
+		} catch (IOException k) {
+			System.out.println("CLIENT: Client IO Exception" + k);
+		}
+	}
+	
+	private static ByteBuffer trimExcessBytes(
+			ByteBuffer buffer, int totalBytes, int packetSize) {
+		int bytesRem = totalBytes % packetSize;
+		System.out.println("bytesRem: " + bytesRem);
+		ByteBuffer temp = ByteBuffer.allocate(bytesRem);
+		buffer.flip();
+		for (int i = 4; i < bytesRem + 4; i++) {
+			temp.put(buffer.get(i));
+		}
+		// Add 4 initial empty bytes to fit in with other packets
+		buffer = ByteBuffer.allocate(bytesRem + 4);
+		temp.position(0);
+		buffer.position(4);
+		buffer.put(temp);
+		return buffer;
+	}
+	
+	private static int determineAckNum(int ackNum, int packetsLength) {
+		return 0;
+	}
 	private static String extractExtension(final String fileName) {
 		String fileExtension = "";
 		for (int i = fileName.length() - 1; i > 0; i--) {
@@ -194,14 +246,23 @@ public class Client {
 			System.out.println("Oh no!");
 		}
 	}
-	/*
+	
+	private static boolean checkPortInput(int p) {
+		if (p < 0 || p > 65535) {
+			System.out.println("Incorrect port input, try again.");
+			return false;
+		}
+		return true;
+	}
+	
 	private static boolean checkInput(String str) {
 		for (int i = 0; i < str.length(); i++) {
-			if (!(str.charAt(i) >= 0) || str.charAt(i) != '.') {
+			if (!(str.charAt(i) >= 0) || (str.charAt(i) != '.')) {
+				System.out.println("Incorrect format, try again.");
 				return false;
 			} 
 		}
 		return true;
 	}
-	*/
+	
 }
--- UDPFileTransfer/src/filetransfer/Server.java
+++ UDPFileTransfer/src/filetransfer/Server.java
@@ -17,10 +17,16 @@ public class Server {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("SERVER: Enter a port number (Leave blank for default): ");
 		String p = scan.nextLine();
-		System.out.println("SERVER: Port set.");
 		if (p.equals("")) {
 			p = "9870";
 		}	
+		if (!(checkPortInput(Integer.parseInt(p)))) {
+			scan.close();
+			System.exit(0);
+		}
+		
+		System.out.println("SERVER: Port set.");
+		
 		int port = Integer.parseInt(p);
 		scan.close();
 		
@@ -38,9 +44,9 @@ public class Server {
 			ArrayList<byte[]> packets = new ArrayList<byte[]>();
 			int floor = 0;
 			int winSize = 5;
-			int high = winSize;
+			int high = winSize; // account for zero
 			int low = 0;
-			int packetSize = 14;
+			int packetSize = 1024; // success dependent upon packetSize???
 			int totalPackets = 0;
 			
 			// Waits until a non-empty file name is received, then exits.
@@ -61,17 +67,19 @@ public class Server {
 					if (!fileName.isEmpty()) {
 						// Calculate total number of packets, which will be sent to client.
 						file = new File(fileName);
-						long totalBytes = file.length();
-						if (totalBytes % packetSize != 0) {
-							totalPackets = (int)((totalBytes/packetSize) + 0.5);
+						int totalBytes = (int)file.length(); // long is not decimal
+						if (totalBytes % packetSize != 0) { // if not a whole number
+							totalPackets = (int)
+									Math.round(((double) totalBytes/packetSize) + 0.5);
 						} else {
-							totalPackets = (int)(totalBytes/packetSize);
+							totalPackets = totalBytes/packetSize;
 						}
 						System.out.println("total packets: " + totalPackets);
 						System.out.println("SERVER: File name received.");
-						buffer = ByteBuffer.allocate(8);
+						buffer = ByteBuffer.allocate(12);
 						buffer.putInt(packetSize);
 						buffer.putInt(totalPackets);
+						buffer.putInt(totalBytes);
 						buffer.flip();
 						c.send(buffer, clientAddr);
 						System.out.println("SERVER: File name received ACK sent.");
@@ -80,12 +88,6 @@ public class Server {
 				}
 			}
 			/*
-			 * 1/28
-			 * variables: Floor, winLow, winHigh, winSize = 5, ackNumber = last packet acked
-			 * Window size shortens and grows back up to max
-			 * Sequence number is 1 greater than window size. Seq = 6;
-			 * Numbers are in three possible states: in window and ready to be sent, sent and not acked, or sent and acked.
-			 * 
 			 * When transmitted, low++. When acked, high++. 
 			 * use %winSize to implement sequence numbers.  
 			 * Could use an arrayList of byte arrays and empty previous byte arrays, but keep them in memory. 
@@ -100,16 +102,19 @@ public class Server {
 				
 				int bytesRead = 0;
 				while (bytesRead != -1) {
-					while (high > low && ((bytesRead >= packetSize) || (packets.size() == 0))) { // what if you run out of data in the file?
+					while (high > low && ((bytesRead >= packetSize) || 
+							(packets.size() == 0))) {
+					//while (high > low) {
 						byte[] b = new byte[packetSize];
 						// Allows the last packet to be sized flexibly
+						System.out.println(in.available());
 						if (in.available() < packetSize) {
 							System.out.println("last packet");
 							b = new byte[in.available()];
 						}
 						bytesRead = in.read(b); // -1 if end of file
 
-						System.out.println("Bytes read: " + bytesRead);
+						//System.out.println("Bytes read: " + bytesRead);
 						packets.add(b);
 						buffer = ByteBuffer.allocate(packetSize + 4);
 						buffer.putInt(packets.size() - 1);
@@ -117,7 +122,7 @@ public class Server {
 						buffer.flip(); // to read mode
 						int bytesSent = c.send(buffer, clientAddr);
 						System.out.println("Bytes sent: " + bytesSent);
-						System.out.println("SERVER: Packet# " + packets.size() + " sent.");
+						System.out.println("SERVER: Packet# " + (packets.size() - 1) + " sent.");
 						low++;
 					} 
 					
@@ -137,6 +142,7 @@ public class Server {
 							high += (ack - floor);
 						}
 						System.out.println("high: " + high);
+						System.out.println("");
 						prevFloor = floor;
 						floor = ack;
 						
@@ -154,12 +160,20 @@ public class Server {
 				}
 				in.close();
 			} else {
-				System.out.println("File not found");
+				System.out.println("File not found!");
 				//str = "file not found";
 				//sc.write(buffer);
 			}
 		} catch (IOException e) {
 			System.out.println("Server IO Exception");
 		}
+	}
+	
+	private static boolean checkPortInput(int p) {
+		if (p < 0 || p > 65535) {
+			System.out.println("Incorrect port input, try again.");
+			return false;
+		}
+		return true;
 	}
 }
